ffa548d03bf1629fa5bc0553ea9a03afabe8aeaf
unknown <cannicam@gmail.com>
2020-11-12 14:16:23 -0600

    completed week 1

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,57 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            var answ = minuend - subtrahend;
+            return answ;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            var a = number1;
+            var b = number2;
+            var answ = a + b;
+            return answ;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if(number1 > number2)
+            {
+                return number2;
+            }
+            else
+            {
+                return number1;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            var prod = factor1 * factor2;
+            return prod;
+
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            var x = nameOfPerson;
+            if (x == "")
+            {
+                var a = "Hello!";
+                return a;
+            }
+            else
+            {
+                var b = "Hello, " + x + "!";
+                return b;
+            }
+            
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            var a = "HEY!";
+            return a;
         }
     }
 }
